3bd454284475c45a9474ccce9d08cf81bff6ddfc
Takaya Saeki <abc.tkys+pub@gmail.com>
2017-12-16 16:16:26 +0900

    Add a missing file

---
+++ src/fs/fs.c
@@ -0,0 +1,372 @@
+/*-
+ * Copyright (c) 2016 Yuichi Nishiwaki and Takaya Saeki
+ * Copyright (c) 1994-1995 Søren Schmidt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "common.h"
+#include "noah.h"
+
+#include "linux/common.h"
+#include "linux/time.h"
+#include "linux/fs.h"
+#include "linux/misc.h"
+#include "linux/errno.h"
+#include "linux/ioctl.h"
+#include "linux/termios.h"
+#include "linux/socket.h"
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <sys/select.h>
+#include <sys/poll.h>
+#include <sys/mount.h>
+#include <sys/syslimits.h>
+#include <dirent.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+
+#include <mach-o/dyld.h>
+
+struct dir {
+  int fd;
+};
+
+struct path {
+  struct fs *fs;
+  struct dir *dir;
+  char subpath[LINUX_PATH_MAX];
+};
+
+struct fs {
+  struct fs_operations *ops;
+};
+
+struct fs_operations {
+  int (*openat)(struct fs *fs, struct dir *dir, const char *path, int flags, int mode); /* TODO: return struct file * instaed of file descripter */
+  int (*symlinkat)(struct fs *fs, const char *target, struct dir *dir, const char *name);
+  int (*faccessat)(struct fs *fs, struct dir *dir, const char *path, int mode);
+  int (*renameat)(struct fs *fs, struct dir *dir1, const char *from, struct dir *dir2, const char *to);
+  int (*linkat)(struct fs *fs, struct dir *dir1, const char *from, struct dir *dir2, const char *to, int flags);
+  int (*unlinkat)(struct fs *fs, struct dir *dir, const char *path, int flags);
+  int (*readlinkat)(struct fs *fs, struct dir *dir, const char *path, char *buf, int bufsize);
+  int (*mkdirat)(struct fs *fs, struct dir *dir, const char *path, int mode);
+  /* inode operations */
+  int (*fstatat)(struct fs *fs, struct dir *dir, const char *path, struct l_newstat *stat, int flags);
+  int (*statfs)(struct fs *fs, struct dir *dir, const char *path, struct l_statfs *buf);
+  int (*fchownat)(struct fs *fs, struct dir *dir, const char *path, l_uid_t uid, l_gid_t gid, int flags);
+  int (*fchmodat)(struct fs *fs, struct dir *dir, const char *path, l_mode_t mode);
+};
+
+struct file {
+  struct file_operations *ops;
+  int fd;
+};
+
+struct file_operations {
+  int (*readv)(struct file *f, struct iovec *iov, size_t iovcnt);
+  int (*writev)(struct file *f, const struct iovec *iov, size_t iovcnt);
+  int (*close)(struct file *f);
+  int (*ioctl)(struct file *f, int cmd, uint64_t val0);
+  int (*lseek)(struct file *f, l_off_t offset, int whence);
+  int (*getdents)(struct file *f, char *buf, uint count, bool is64);
+  int (*fcntl)(struct file *f, unsigned int cmd, unsigned long arg);
+  int (*fsync)(struct file *f);
+  /* inode operations */
+  int (*fstat)(struct file *f, struct l_newstat *stat);
+  int (*fstatfs)(struct file *f, struct l_statfs *buf);
+  int (*fchown)(struct file *f, l_uid_t uid, l_gid_t gid);
+  int (*fchmod)(struct file *f, l_mode_t mode);
+};
+
+int
+darwinfs_openat(struct fs *fs, struct dir *dir, const char *path, int l_flags, int mode)
+{
+  int flags = linux_to_darwin_o_flags(l_flags);
+  return syswrap(openat(dir->fd, path, flags, mode));
+}
+
+int
+darwinfs_close(struct file *file)
+{
+  return syswrap(close(file->fd));
+}
+
+int
+darwinfs_writev(struct file *file, const struct iovec *iov, size_t iovcnt)
+{
+  return syswrap(writev(file->fd, iov, iovcnt));
+}
+
+int
+darwinfs_readv(struct file *file, struct iovec *iov, size_t iovcnt)
+{
+  return syswrap(readv(file->fd, iov, iovcnt));
+}
+
+#define LOOKUP_NOFOLLOW   0x0001
+#define LOOKUP_DIRECTORY  0x0002
+/* #define LOOKUP_CONTINUE   0x0004 */
+/* #define LOOKUP_AUTOMOUNT  0x0008 */
+/* #define LOOKUP_PARENT     0x0010 */
+/* #define LOOKUP_REVAL      0x0020 */
+
+#define LOOP_MAX 20
+
+int
+resolve_path(const struct dir *parent, const char *name, int flags, struct path *path, int loop)
+{
+  static struct fs_operations ops = {
+    darwinfs_openat,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+  };
+  static struct fs darwinfs = {
+    .ops = &ops,
+  };
+  struct fs *fs = &darwinfs;
+
+  if (loop > LOOP_MAX)
+    return -LINUX_ELOOP;
+
+  struct dir dir = *parent;
+
+  /* resolve mountpoints */
+  if (*name == '/') {
+    if (name[1] == '\0') {
+      dir.fd = proc.fileinfo.rootfd;
+      strcpy(path->subpath, ".");
+      goto out;
+    }
+    if (strncmp(name, "/Users", sizeof "/Users" - 1) && strncmp(name, "/Volumes", sizeof "/Volumes" - 1) && strncmp(name, "/dev", sizeof "/dev" - 1) && strncmp(name, "/tmp", sizeof "/tmp" - 1) && strncmp(name, "/private", sizeof "/private" - 1)) {
+      dir.fd = proc.fileinfo.rootfd;
+      name++;
+    }
+  }
+
+  /* resolve symlinks */
+  char *sp = path->subpath;
+  *sp = 0;
+  const char *c = name;
+  assert(*c);
+  while (*c) {
+    while (*c && *c != '/') {
+      *sp++ = *c++;
+    }
+    *sp = 0;
+    if ((flags & LOOKUP_NOFOLLOW) == 0) {
+      // TODO: resolve symlinks
+    }
+    if (*c) {
+      *sp++ = *c++;
+    }
+    *sp = 0;
+  }
+
+ out:
+  path->fs = fs;
+  path->dir = malloc(sizeof(struct dir));
+  path->dir->fd = dir.fd;
+  return 0;
+}
+
+int
+vfs_grab_dir(int dirfd, const char *name, int flags, struct path *path)
+{
+  struct dir dir;
+
+  if (flags & ~(LOOKUP_NOFOLLOW | LOOKUP_DIRECTORY)) {
+    return -LINUX_EINVAL;
+  }
+
+  if (*name == 0) {
+    return -LINUX_ENOENT;
+  }
+
+  if (dirfd == LINUX_AT_FDCWD) {
+    dir.fd = AT_FDCWD;
+  } else {
+    dir.fd = dirfd;
+    // if (!in_userfd(dir.fd)) {
+    //   return -LINUX_EBADF;
+    // }
+  }
+  return resolve_path(&dir, name, flags, path, 0);
+}
+
+void
+vfs_ungrab_dir(struct path *path)
+{
+  free(path->dir);
+}
+
+static int
+do_openat(int dirfd, const char *name, int flags, int mode)
+{
+  int lkflag = 0;
+  if (flags & LINUX_O_NOFOLLOW) {
+    lkflag |= LOOKUP_NOFOLLOW;
+  }
+  if (flags & LINUX_O_DIRECTORY) {
+    lkflag |= LOOKUP_DIRECTORY;
+  }
+
+  struct path path;
+  int r = vfs_grab_dir(dirfd, name, lkflag, &path);
+  if (r < 0) {
+    return r;
+  }
+  r = path.fs->ops->openat(path.fs, path.dir, path.subpath, flags, mode);
+  vfs_ungrab_dir(&path);
+  return r;
+}
+
+int
+do_close(struct fdtable *table, int fd)
+{
+  if (fd < table->start || fd >= table->start + table->size) {
+    return -LINUX_EBADF;
+  }
+//   if (!test_fdbit(table, table->open_fds, fd)) {
+//     return -LINUX_EBADF;
+//   }
+//   struct file *file = &table->files[fd - table->start];
+//   assert(file);
+  int n = syswrap(close(fd));
+//   clear_fdbit(table, table->open_fds, fd);
+//   clear_fdbit(table, table->cloexec_fds, fd);
+  return n;
+}
+
+int
+vkern_openat(int atdirfd, const char *name, int flags, int mode)
+{
+  int ret;
+
+  pthread_rwlock_wrlock(&proc.fileinfo.fdtable_lock);
+  int fd = do_openat(atdirfd, name, flags, mode);
+  if (fd < 0) {
+    ret = fd;
+    goto out;
+  }
+  ret = fd;
+//  ret = vkern_dup_fd(fd, flags & LINUX_O_CLOEXEC);
+  close(fd);
+
+out:
+  pthread_rwlock_unlock(&proc.fileinfo.fdtable_lock);
+  return ret;
+}
+
+int
+vkern_open(const char *path, int l_flags, int mode)
+{
+  return vkern_openat(LINUX_AT_FDCWD, path, l_flags, mode);
+}
+
+int
+vkern_close(int fd)
+{
+  pthread_rwlock_wrlock(&proc.fileinfo.fdtable_lock);
+  int n = do_close(&proc.fileinfo.vkern_fdtable, fd);
+  pthread_rwlock_unlock(&proc.fileinfo.fdtable_lock);
+  return n;
+}
+
+DEFINE_SYSCALL(read, int, fd, gaddr_t, buf_ptr, size_t, size)
+{
+  int r;
+  char *buf = malloc(size);
+  /*
+  struct file *file = get_file(fd);
+  if (file == NULL) {
+    r = -LINUX_EBADF;
+    goto out;
+  }
+  if (file->ops->readv == NULL) {
+    r = -LINUX_EBADF;
+    goto out;
+  }
+  */
+  struct iovec iov = { buf, size };
+  // r = file->ops->readv(file, &iov, 1);
+  r = syswrap(readv(fd, &iov, 1));
+  if (r < 0) {
+    goto out;
+  }
+  if (copy_to_user(buf_ptr, buf, r)) {
+    r = -LINUX_EFAULT;
+    goto out;
+  }
+out:
+  free(buf);
+  return r;
+}
+
+DEFINE_SYSCALL(write, int, fd, gaddr_t, buf_ptr, size_t, size)
+{
+  int r;
+  char *buf = malloc(size);
+  if (copy_from_user(buf, buf_ptr, size)) {
+    r = -LINUX_EFAULT;
+    goto out;
+  }
+  /*struct file *file = get_file(fd);
+  if (file == NULL) {
+    r = -LINUX_EBADF;
+    goto out;
+  }
+  if (file->ops->writev == NULL) {
+    r = -LINUX_EBADF;
+    goto out;
+  }*/
+  struct iovec iov = { buf, size };
+  // r =  file->ops->writev(file, &iov, 1);
+  r = syswrap(writev(fd, &iov, 1));
+out:
+  free(buf);
+  return r;
+}
