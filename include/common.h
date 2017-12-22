1f4039153d69215a782f0d205a2cdfa000fb5ad2
Takaya Saeki <abc.tkys+pub@gmail.com>
2017-12-22 21:16:03 +0900

    Workaound for VC++'s macro expansion bug

--- include/common.h
+++ include/common.h
@@ -4,15 +4,17 @@
  * ``_MAP(f, t0, v0, t1, v1, ...)'' => ``f(t0, v0), f(t1, v1), ...''
  */
 
-#define _MAP(f,...) _DISPATCH(__VA_ARGS__,_MAP7,,_MAP6,,_MAP5,,_MAP4,,_MAP3,,_MAP2,,_MAP1,_MAP0)(f,__VA_ARGS__)
+#define _EXPAND_VA_ARGS(x) x  // Workaround for VC++'s macro expansion bug. Force it to recognize
+                            // the expansion result of __VA_ARGS__ as multiple inputs
+#define _MAP(f,...) _EXPAND_VA_ARGS(_DISPATCH(__VA_ARGS__,_MAP7,,_MAP6,,_MAP5,,_MAP4,,_MAP3,,_MAP2,,_MAP1,_MAP0)(f,__VA_ARGS__))
 #define _MAP0(f,...)
 #define _MAP1(f,t,v)     f(t,v)
-#define _MAP2(f,t,v,...) f(t,v), _MAP1(f,__VA_ARGS__)
-#define _MAP3(f,t,v,...) f(t,v), _MAP2(f,__VA_ARGS__)
-#define _MAP4(f,t,v,...) f(t,v), _MAP3(f,__VA_ARGS__)
-#define _MAP5(f,t,v,...) f(t,v), _MAP4(f,__VA_ARGS__)
-#define _MAP6(f,t,v,...) f(t,v), _MAP5(f,__VA_ARGS__)
-#define _MAP7(f,t,v,...) f(t,v), _MAP6(f,__VA_ARGS__)
+#define _MAP2(f,t,v,...) f(t,v), _EXPAND_VA_ARGS(_MAP1(f,__VA_ARGS__))
+#define _MAP3(f,t,v,...) f(t,v), _EXPAND_VA_ARGS(_MAP2(f,__VA_ARGS__))
+#define _MAP4(f,t,v,...) f(t,v), _EXPAND_VA_ARGS(_MAP3(f,__VA_ARGS__))
+#define _MAP5(f,t,v,...) f(t,v), _EXPAND_VA_ARGS(_MAP4(f,__VA_ARGS__))
+#define _MAP6(f,t,v,...) f(t,v), _EXPAND_VA_ARGS(_MAP5(f,__VA_ARGS__))
+#define _MAP7(f,t,v,...) f(t,v), _EXPAND_VA_ARGS(_MAP6(f,__VA_ARGS__))
 #define _DISPATCH(T0,V0,T1,V1,T2,V2,T3,V3,T4,V4,T5,V5,T6,V6,X,...) X
 
 #define MK_DECL(t,v) t v
@@ -20,23 +22,24 @@
 #define MK_CAST(t,v) (t) temp__##v
 
 #define DECLARE_SCFUNCT(name, ...)                      \
-  uint64_t sys_##name(_MAP(MK_DECL, __VA_ARGS__));
+  uint64_t sys_##name(_EXPAND_VA_ARGS(_MAP(MK_DECL, __VA_ARGS__)));
 
 #define DEFINE_SCWRAPPER(name, ...)                                                \
-  uint64_t _sys_##name(_MAP(MK_TEMP,__VA_ARGS__)) {                                 \
-    meta_strace_pre(LSYS_##name, #name, _MAP(MK_STRACE_CALL, ##__VA_ARGS__, 0, 0));     \
-    uint64_t ret = sys_##name(_MAP(MK_CAST,__VA_ARGS__));                         \
-    meta_strace_post(LSYS_##name, #name, ret, _MAP(MK_STRACE_CALL, ##__VA_ARGS__, 0, 0));                                       \
+  uint64_t _sys_##name(_EXPAND_VA_ARGS(_MAP(MK_TEMP,__VA_ARGS__))) {                                 \
+    meta_strace_pre(LSYS_##name, #name, _EXPAND_VA_ARGS(_MAP(MK_STRACE_CALL, ##__VA_ARGS__, 0, 0)));     \
+    uint64_t ret = sys_##name(_EXPAND_VA_ARGS(_MAP(MK_CAST,__VA_ARGS__)));                         \
+    meta_strace_post(LSYS_##name, #name, ret, _EXPAND_VA_ARGS(_MAP(MK_STRACE_CALL, ##__VA_ARGS__, 0, 0)));                                       \
     return ret;                                                                    \
   }
 
 #define DEFINE_SCFUNCT(name, ...)                       \
-  uint64_t sys_##name(_MAP(MK_DECL, __VA_ARGS__))
+  uint64_t sys_##name(_EXPAND_VA_ARGS(_MAP(MK_DECL, __VA_ARGS__)))
 
 #define DEFINE_SYSCALL(name, ...)               \
-  DECLARE_SCFUNCT(name, ##__VA_ARGS__)          \
-  DEFINE_SCWRAPPER(name, ##__VA_ARGS__)         \
-  DEFINE_SCFUNCT(name, ##__VA_ARGS__)
+  _EXPAND_VA_ARGS(DECLARE_SCFUNCT(name, ##__VA_ARGS__))          \
+  _EXPAND_VA_ARGS(DEFINE_SCWRAPPER(name, ##__VA_ARGS__))         \
+  _EXPAND_VA_ARGS(DEFINE_SCFUNCT(name, ##__VA_ARGS__))
+
 
 /*
  * strace related macros
