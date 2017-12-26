1ed4ec7f5986d9c784c01402ce03b3d3a62ebb95
Takaya Saeki <abc.tkys+pub@gmail.com>
2017-12-26 12:16:42 +0900

    Suppress a warning by MSVC

--- src/meta_strace.c
+++ src/meta_strace.c
@@ -193,10 +193,12 @@ meta_strace_hook *strace_pre_hooks[NR_SYSCALLS] = {
 //  [LSYS_sendto] = trace_sendto_pre,
 //  [LSYS_execve] = trace_execve_pre,
 //  [LSYS_rt_sigprocmask] = trace_rt_sigprocmask_post,
+  NULL
 };
 meta_strace_hook *strace_post_hooks[NR_SYSCALLS] = {
 //  [LSYS_read] = trace_read_post,
 //  [LSYS_recvfrom] = trace_recvfrom_post,
 //  [LSYS_rt_sigprocmask] = trace_rt_sigprocmask_pre,
+  NULL
 };
 
