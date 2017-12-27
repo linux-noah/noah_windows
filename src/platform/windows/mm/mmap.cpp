e550cbddaa6e407756fa73cb82c30d0b04283e55
Takaya Saeki <abc.tkys+pub@gmail.com>
2017-12-27 16:27:43 +0900

    [WIP] Implementing platform-mmap functions on Windows

--- src/platform/windows/mm/mmap.cpp
+++ src/platform/windows/mm/mmap.cpp
@@ -1,5 +1,8 @@
 
 #include <cerrno>
+#include <Windows.h>
+#include <boost/interprocess/file_mapping.hpp>
+#include <boost/interprocess/mapped_region.hpp>
 
 extern "C" {
 #include "linux/errno.h"
@@ -7,6 +10,7 @@ extern "C" {
 #include "mm.h"
 }
 
+namespace bi = boost::interprocess;
 
 int
 platform_alloc_mem(void **ret, size_t size, int prot)
@@ -27,11 +31,41 @@ platform_alloc_shared_mem(void **ret, size_t size, int prot)
 int
 platform_alloc_filemapped_mem(void **ret, ssize_t size, int prot, bool writes_back, off_t offset, const char *path)
 {
-  return -LINUX_EINVAL;
+  bi::mode_t mode = bi::read_only;
+  if (prot == PROT_READ) mode = bi::read_only;
+  if (prot & PROT_WRITE) {
+    if (writes_back) {
+      mode = bi::read_write;
+    } else {
+      mode = bi::copy_on_write;
+    }
+  }
+
+  try {
+    bi::file_mapping mapping(path, mode);
+    bi::mapped_region region;
+    if (size == -1) {
+      region = bi::mapped_region(mapping, mode, offset);
+      size = region.get_size();
+    } else {
+      region = bi::mapped_region(mapping, mode, offset);
+    }
+    *ret = region.get_address();
+  } catch (const bi::interprocess_exception &e) {
+    // TODO: neat error conversion
+    return -LINUX_ENOENT;
+  } catch (...) {
+    return -LINUX_ENOENT;
+  }
+  return size;
 }
 
 int
 platform_unmap_mem(void *mem, size_t size)
 {
-  return -LINUX_EINVAL;
+  // TODO: unmap of filemapped memory
+  if (!VirtualFree(mem, size, MEM_DECOMMIT)) {
+    return -native_to_linux_errno(_doserrno);
+  }
+  return 0;
 }
